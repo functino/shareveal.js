03af43619a8f8f0caaa8d276fc0e0c2d9e5c3db6
Andreas <andreas_geier@web.de>
2012-10-07 17:37:15 +0200

    Added webcam swipe detectino to move forward in a presenation

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
 var Faye   = require('faye');
+var http = require("http");
 var eco = require("eco");
 var fs  = require("fs");
 
 var fayeServer = new Faye.NodeAdapter({mount: '/faye'});
 var express = require("express");
-var server = express.createServer();
+var app = express();
 
-
-server.configure(function() {
-    return server.use(express.static(__dirname + "/public"));
-  });
-fayeServer.attach(server);
-server.listen(3015);
+app.configure(function() {
+    return app.use(express.static(__dirname + "/public"));
+});
 
 var client = new Faye.Client('http://localhost:3015/faye');
 
-var template = fs.readFileSync(__dirname + "/template.html", "utf-8");
-var remoteTemplate = fs.readFileSync(__dirname + "/remote.html", "utf-8");
-var slides = fs.readFileSync(__dirname + "/slides.html", "utf-8");
+// var template = fs.readFileSync(__dirname + "/template.html", "utf-8");
+// var remoteTemplate = fs.readFileSync(__dirname + "/remote.html", "utf-8");
+// var slides = fs.readFileSync(__dirname + "/slides.html", "utf-8");
 
-server.get("/", function(req, res, next) {
+app.get("/", function(req, res, next) {
+	var template = fs.readFileSync(__dirname + "/template.html", "utf-8");
+	var slides = fs.readFileSync(__dirname + "/slides.html", "utf-8");
 	res.send(eco.render(template, {slides: slides, isMaster: false}));
 });
-server.get("/master", function(req, res, next) {
+app.get("/master", function(req, res, next) {
+	var template = fs.readFileSync(__dirname + "/template.html", "utf-8");
+	var slides = fs.readFileSync(__dirname + "/slides.html", "utf-8");
 	res.send(eco.render(template, {slides: slides, isMaster: true}));
 });
-server.get("/remote", function(req, res, next) {
+app.get("/remote", function(req, res, next) {
+	var remoteTemplate = fs.readFileSync(__dirname + "/remote.html", "utf-8");
 	res.send(eco.render(remoteTemplate, {}));
 });
 
-server.get("/remote/next", function(req, res, next) {
+app.get("/remote/next", function(req, res, next) {
 	client.publish('/reveal/master/next', {});
 	res.send("")
 });
-server.get("/remote/back", function(req, res, next) {
+app.get("/remote/back", function(req, res, next) {
 	client.publish('/reveal/master/back', {});
 	res.send("")
 });
 
+var server = http.createServer(app);
+fayeServer.attach(server);
+server.listen(3015);
 
 console.log("Server running")
